594190470ed8256b4faef7e734e4aa113640bc76
hanselblack <hanselblack@hotmai.com>
2017-10-04 00:59:24 +0800

    Text to Speech

--- src/main/java/seedu/address/logic/TextToSpeech.java
+++ src/main/java/seedu/address/logic/TextToSpeech.java
@@ -8,20 +8,19 @@ import javax.speech.synthesis.SynthesizerModeDesc;
 
 public class TextToSpeech {
 	public TextToSpeech (String message){
-		try {
-			// Create a synthesizer for English
-			Synthesizer synth = Central.createSynthesizer(new SynthesizerModeDesc(Locale.ENGLISH));
-			// Get it ready to speak
-			synth.allocate();
-			synth.resume();
-			// Speak the "Hello world" string
-			synth.speakPlainText(message, null);
-			// Wait till speaking is done
-			synth.waitEngineState(Synthesizer.QUEUE_EMPTY);
-			// Clean up
-			synth.deallocate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	    try {
+		    //Create a synthesizer for English
+            Synthesizer synth = Central.createSynthesizer(new SynthesizerModeDesc(Locale.ENGLISH));
+            synth.allocate();
+            synth.resume();
+            //Get it ready to speak
+            synth.speakPlainText(message,null);
+            synth.waitEngineState(Synthesizer.QUEUE_EMPTY);
+            //Clean up
+            synth.deallocate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 }
